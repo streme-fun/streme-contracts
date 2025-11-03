17fd194c446cf11bc4d007ff0f95c86e8200004a
markcarey <mark@markcarey.com>
2025-11-03 09:12:05 -0500

    update abis

--- hardhat.config.js
+++ hardhat.config.js
@@ -27,7 +27,7 @@ module.exports = {
       forking: {
         url: process.env.API_URL_BASE,
         ignoreUnknownTxType: true,
-        blockNumber: 37138651        // assumes Base fork
+        blockNumber: 37481397        // assumes Base fork
       },
       gasMultiplier: 2,
       initialBaseFeePerGas: 0, //14689933,
