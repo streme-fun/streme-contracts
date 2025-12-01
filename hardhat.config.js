7f0a0c693f1e431dcca46a2124024891c665c123
markcarey <mark@markcarey.com>
2025-12-01 09:24:22 -0500

    prebuy tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -27,7 +27,7 @@ module.exports = {
       forking: {
         url: process.env.API_URL_BASE,
         ignoreUnknownTxType: true,
-        blockNumber: 38777563        // assumes Base fork
+        blockNumber: 38778365        // assumes Base fork
       },
       gasMultiplier: 2,
       initialBaseFeePerGas: 0, //14689933,
